859433d6d6cf2992256c76b8a155c93ad9146335
Alexandre Jung <jungalexandre@ymail.com>
2021-08-29 02:13:18 +0200

    :building_construction: improve project structure
    - use cssnano for CSS compression
    - update build process
    - update npm scripts

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,17 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
 
 module.exports = {
     plugins: [
         purgecss({
             content: ['./**/*.html']
-        })
+        }),
+        cssnano({
+            preset: ['default', {
+                discardComments: {
+                    removeAll: true
+                }
+            }]
+        }),
     ]
 }
